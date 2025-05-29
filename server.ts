ef64de6585ec8889e1477f32c277f903b96cb878
niculuka <niculuka@yahoo.com>
2025-05-29 19:46:55 +0300

    updates

--- server.ts
+++ server.ts
@@ -40,7 +40,7 @@ export function app(): Express {
 }
 
 function run(): void {
-    const port = process.env['PORT'] || 4000;
+    const port = process.env['PORT'] || 3008;
 
     // Start up the Node server
     const server = app();
